412f3f3b7e69dceb596cce88a70a872986319d5c
Jeffrey Sackey <zcemjna@ucl.ac.uk>
2023-11-02 10:45:51 +0000

    Put imports on 1 line for test_times Added 3 more unit tests that test: - two time ranges that do not overlap - two time ranges that both contain several intervals each - two time ranges that end exactly at the same time when the other starts

--- test_times.py
+++ test_times.py
@@ -1,10 +1,34 @@
-from times import compute_overlap_time
-from times import time_range
+from times import compute_overlap_time, time_range  # Put all imports from times.py on 1 line
 
 
-def test_given_input():
+def test_different_sizes():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     result = compute_overlap_time(large, short)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
+
+
+def test_no_overlap():
+    periodone = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    periodtwo = time_range("2010-01-12 15:00:00", "2010-01-12 17:00:00")
+    result = compute_overlap_time(periodone, periodtwo)
+    expected = []
+    assert result == expected
+
+
+def test_several_intervals():
+    periodone = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    periodtwo = time_range("2010-01-12 09:00:00", "2010-01-12 12:00:00", 2, 120)
+    result = compute_overlap_time(periodone, periodtwo)
+    expected = [('2010-01-12 10:30:00', '2010-01-12 10:29:00'), ('2010-01-12 10:31:00', '2010-01-12 10:37:00'),
+                ('2010-01-12 10:38:00', '2010-01-12 10:29:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+    assert result == expected
+
+def test_immediate_continue():
+    periodone = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    periodtwo = time_range("2010-01-12 12:00:00", "2010-01-12 15:00:00")
+    result = compute_overlap_time(periodone, periodtwo)
+    expected = [('2010-01-12 12:00:00', '2010-01-12 12:00:00')]
+    assert result == expected
+
--- times.py
+++ times.py
@@ -1,10 +1,16 @@
 import datetime
 
 
+# Takes a start_time,
+# end_time,
+# number of time intervals desired (which splits the time period into multiple time periods),
+# the gap between the intervals (which decides the space between the end of 1 time period to
+# the start of another)
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
-    start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
-    print(start_time_s)
-    end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
+    start_time_s = datetime.datetime.strptime(start_time,
+                                              "%Y-%m-%d %H:%M:%S")  # strptime converts the string to a datetime object
+    end_time_s = datetime.datetime.strptime(end_time,
+                                            "%Y-%m-%d %H:%M:%S")  # strptime converts the string to a datetime object
     d = (end_time_s - start_time_s).total_seconds() / number_of_intervals + gap_between_intervals_s \
         * (1 / number_of_intervals - 1)
     sec_range = [(start_time_s + datetime.timedelta(seconds=i * d + i * gap_between_intervals_s),
@@ -24,4 +30,7 @@ def compute_overlap_time(range1, range2):
 
 
 if __name__ == "__main__":
+    # print(compute_overlap_time(time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"), time_range("2010-01-12 ""15:00:00", "2010-01-12 ""17:00:00")))
+    # print(compute_overlap_time(time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60), time_range("2010-01-12 09:00:00", "2010-01-12 12:00:00", 2, 120)))
+    #print(compute_overlap_time(time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"),time_range("2010-01-12 ""12:00:00", "2010-01-12 ""15:00:00")))
     pass
